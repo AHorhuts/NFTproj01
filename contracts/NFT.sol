c5d431d92a692d8a546083fb75ce935f271c4c8f
yurii <sllouyssgort@gmail.com>
2022-11-02 15:23:48 +0100

    Test added

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,42 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.7.3/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
-import "@openzeppelin/contracts@4.7.3/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Horhuts is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-    
+
     string[] private NFTuri;
 
     constructor(string[] memory _NFTuri) ERC721("Horhuts", "HORH"){
-        require (_NFTuri.length == 25);
+        require(_NFTuri.length == 25, "Invalid URI list size");
         NFTuri = _NFTuri;
     }
-    
-     function Mint() public  onlyOwner {
-        safeMint(msg.sender);
-        
-    }
-    // SafeMint не работает, это я пытался разными способами нужный результат получить, пока что на этом остановился
-    function safeMint(address to) public onlyOwner {
+
+    function mint() public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
-        if (tokenId < 5){
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+        if (tokenId < 5) {
+            _tokenIdCounter.increment();
+            _safeMint(msg.sender, tokenId);
         }
         else {
-        _safeMint(to, _random.tokenId);
+            _safeMint(msg.sender, _random(tokenId));
         }
-     }
-    
-    function _random(uint tokenId) public view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.timestamp,block.difficulty,  
-        msg.sender))) % tokenId;
+    }
+
+    function _random(uint tokenId) public view returns (uint){
+        return uint(
+            keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))
+        ) % tokenId;
+    }
 
     // The following functions are overrides required by Solidity.
 
@@ -45,11 +42,11 @@ contract Horhuts is ERC721, ERC721URIStorage, Ownable {
     }
 
     function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
     {
-        return super.tokenURI(tokenId);
+        return NFTuri[tokenId];
     }
 }
---
+++ test/unit/NFT.spec.ts
@@ -0,0 +1,56 @@
+import {assert, expect} from "chai"
+import { BigNumber } from "ethers"
+import { deployments, network, ethers } from "hardhat"
+import { developmentChains } from "../../helper-hardhat-config"
+import {Horhuts, Horhuts__factory, MockV3Aggregator, PriceConsumerV3} from "../../typechain"
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import {TransferEvent} from "../../typechain/contracts/NFT.sol/Horhuts";
+
+describe("NFT test", async () => {
+  // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+
+    const uris = [...Array(25).keys()].map( item => item.toFixed())
+
+    let contract: Horhuts
+    let deployer: SignerWithAddress
+
+    beforeEach(async () => {
+        // await deployments.fixture(["mocks", "feed"])
+        [deployer] = await ethers.getSigners()
+        const factory = await ethers.getContractFactory("Horhuts") as Horhuts__factory
+        contract = await factory.deploy(uris)
+        await contract.deployed()
+    })
+
+    it("Initial Mint test", async () => {
+        await expect(contract.ownerOf(0)).revertedWith('ERC721: invalid token ID')
+
+        const tx = await contract.mint()
+        const receipt = await tx.wait()
+
+        console.log(receipt.gasUsed.toString())
+
+        expect(await contract.ownerOf(0)).equal(deployer.address)
+        await expect(contract.ownerOf(1)).revertedWith('ERC721: invalid token ID')
+    })
+
+    it("multiply mint test", async () => {
+        await contract.mint()
+        await contract.mint()
+        await contract.mint()
+        await contract.mint()
+        await contract.mint()
+
+        expect(await contract.tokenURI(0)).equal('0')
+        expect(await contract.tokenURI(1)).equal('1')
+        expect(await contract.tokenURI(2)).equal('2')
+        expect(await contract.tokenURI(3)).equal('3')
+        expect(await contract.tokenURI(4)).equal('4')
+
+        const tx = await contract.mint()
+        // const receipt = await tx.wait()
+        // const event: TransferEvent = receipt.events![0] as TransferEvent
+        // console.log(event)
+        //.emit(contract, 'Transfer')
+    })
+})
