a7daf2222b0fffed6fbc855118dfee883498c2f7
AHorhuts <sashahorhuts@gmail.com>
2022-11-06 00:02:14 +0100

    setTokenURI function added

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -10,14 +10,25 @@ contract Horhuts is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    
+    mapping(uint => uint) private _availableTokenId;
+    uint256 private _numAvailableTokens;
 
+     // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
+
+    // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+    
     string[] private NFTuri;
-
+    
+    
     constructor(string[] memory _NFTuri) ERC721("Horhuts", "HORH"){
         require(_NFTuri.length == 25, "Invalid URI list size");
         NFTuri = _NFTuri;
+        _numAvailableTokens = _NFTuri.length;
     }
-
+      
     function mint() public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         if (tokenId < 5) {
@@ -49,4 +60,14 @@ contract Horhuts is ERC721, ERC721URIStorage, Ownable {
     {
         return NFTuri[tokenId];
     }
-}
+
+    function setTokenURI(uint tokenId, string memory _tokenURI) public onlyOwner{
+        require(!_exists(tokenId), "You can't change URI of already existing NFT");
+        NFTuri[tokenId] = _tokenURI;
+        
+    }
+    
+}    
+    
+    
+
